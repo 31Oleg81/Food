7bb86aea6c650102de284ddaedced315d8ef4178
Oleg <cot.100@mail.ru>
2022-03-11 15:57:26 +0300

    timer Modal

--- Food/js/script.js
+++ Food/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.addEventListener('DOMContentLoaded', () => {
 
 	//Tabs
@@ -41,7 +40,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	//Timer
 
-	const deadline = '2022-03-08';
+	const deadline = '2023-03-25';
 
 	function getTimeRemaining(endtime) {
 		const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -99,14 +98,18 @@ window.addEventListener('DOMContentLoaded', () => {
 		  modal = document.querySelector('.modal'),
 		  modalCloseBtn = document.querySelector('[data-close]');
 
-	modalTrigger.forEach(btn => {
-		btn.addEventListener('click', () => {
+
+	function openModal() {
 		modal.classList.add('show');
 		modal.classList.remove('hide');
-		// modal.classList.toggle('hide');
 		document.body.style.overflow = 'hidden';
-		});
+		clearInterval(modalTimerId);
+	}
+
+	modalTrigger.forEach(btn => {
+		btn.addEventListener('click', openModal);
 	});
+	
 	function closeModal() {
 		modal.classList.add('hide');
 		modal.classList.remove('show');
@@ -128,99 +131,16 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-});
-=======
-window.addEventListener('DOMContentLoaded', () => {
+	// Таймер вывода модального окна
+	const modalTimerId =setTimeout(openModal, 6000);
 
-	//Tabs
-	const tabs = document.querySelectorAll('.tabheader__item'),
-		  tabsGontent = document.querySelectorAll('.tabcontent'),
-		  tabsParent = document.querySelector('.tabheader__items');
-
-	function hideTabContent() {
-		tabsGontent.forEach(item => {
-			item.style.display = 'none';
-		});
-
-		tabs.forEach(item => {
-			item.classList.remove('tabheader__item_active');
-		});
-	}
-
-	function showTabContent(i = 0) {
-		tabsGontent[i].style.display = 'block';
-		tabs[i].classList.add('tabheader__item_active');
-	}
-
-	hideTabContent();
-	showTabContent();
-
-	tabsParent.addEventListener('click', (event) => {
-		const target = event.target;
-
-		if (target && target.classList.contains('tabheader__item')) {
-			tabs.forEach((item, i) => {
-				if (target == item) {
- 					hideTabContent();
-					showTabContent(i);
-				}
-			});
-		}
-	});
-
-	//Timer
-
-	const deadline = '2022-03-08';
-
-	function getTimeRemaining(endtime) {
-		const t = Date.parse(endtime) - Date.parse(new Date()),
-			  days = Math.floor((t / (1000 * 60 * 60 * 24))),
-			  hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-			  minutes = Math.floor((t / (1000 * 60) % 60)),
-			  seconds = Math.floor((t / 1000)  % 60);
-
-		return {
-			'total': t,
-			'days': days,
-			'hours': hours,
-			'minutes': minutes,
-			'seconds': seconds	
-		};
-	}
-
-	function getZero(num) {
-		if (num >= 0 && num < 10) {
-			return '0' + num;
-		} else {
-			return num;
+	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+			openModal();
+			window.removeEventListener('scroll', showModalByScroll);
 		}
 	}
 
-	function setClock(selector, endtime) {
-		const timer = document.querySelector(selector),
-			  days = timer.querySelector('#days'),
-			  hours = timer.querySelector('#hours'),
-			  minutes = timer.querySelector('#minutes'),
-			  seconds = timer.querySelector('#seconds'),
-			  timeInterval = setInterval(updateClock, 1000);
-
-		updateClock();
-
-		function updateClock() {
-			const t = getTimeRemaining(endtime);
-
-			days.innerHTML = getZero(t.days);
-			hours.innerHTML = getZero(t.hours);
-			minutes.innerHTML = getZero(t.minutes);
-			seconds.innerHTML = getZero(t.seconds);
-
-			if (t.total <=0) {
-				clearInterval(timeInterval);
-			}
-		}
-	}
-
-	setClock('.timer', deadline);
+	window.addEventListener('scroll', showModalByScroll);
 
 });
->>>>>>> master
